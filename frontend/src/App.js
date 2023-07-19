2192441e765055691e85cf6636c0646386dcec6c
christopher <christopher083ade@gmail.com>
2023-07-19 15:10:24 +0700

    add input form design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,21 @@ import React,{Component} from 'react';
 
 import Navigation from './components/Navigation/navigation';
 import Logo from './components/Logo/logo';
+import ImageLinkForm from './components/ImageLinkForm/imagelinkform';
+import Rank from './components/Rank/rank';
 
 import './App.css';
+import ParticlesBg from 'particles-bg'
 
 class App extends Component {
   render(){
     return (
       <div className="App">
+        <ParticlesBg type="fountain" bg={true} />
         <Navigation />
         <Logo />
+        <Rank />
+        <ImageLinkForm />
       </div>
     )
   }
