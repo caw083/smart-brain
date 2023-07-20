c376ff3b8aac63e1cf4e153f748031110efa3f2a
christopher <christopher083ade@gmail.com>
2023-07-20 17:31:00 +0700

    add clarifai api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 
+
 import Navigation from './components/Navigation/navigation';
 import Logo from './components/Logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/imagelinkform';
@@ -9,11 +10,53 @@ import FaceRecognition from './components/FaceRecognition/facerecognition';
 import './App.css';
 import ParticlesBg from 'particles-bg'
 
+const returnClarifaiJSONRequest = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = '928db2d6395e48cc9cd35ffebfd536dc';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'hidbg0n4d5tb';       
+  const APP_ID = 'Smart-Brain123';
+  // Change these to whatever model and image URL you want to use
+  const IMAGE_URL = imageUrl;
+  console.log(imageUrl)
+
+
+  const raw = JSON.stringify({
+          "user_app_id": {
+              "user_id": USER_ID,
+              "app_id": APP_ID
+          },
+          "inputs": [
+              {
+                  "data": {
+                      "image": {
+                          "url": IMAGE_URL
+                      }
+                  }
+              }
+          ]
+      });
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+  return requestOptions;
+}
+
+
 class App extends Component {
   constructor(){
     super();
     this.state = {
       input: "",
+      imageUrl: "",
+      box: {}
 
     }
   }
@@ -23,7 +66,14 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    console.log('click')
+    this.setState({imageUrl: this.state.input});
+
+    const MODEL_ID = 'face-detection';
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", returnClarifaiJSONRequest(this.state.input))
+         .then(response => response.text())
+         .then(result => console.log(result))
+         .catch(error => {console.log('error', error); alert("Error format is not correct or Download failed")});
+
 
   }
 
@@ -36,7 +86,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition imageUrl={this.state.input} />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     )
   }
