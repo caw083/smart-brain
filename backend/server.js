b31a3e45850bf57cd240ab7f1a048091961f952a
christopher <christopher083ade@gmail.com>
2023-07-21 23:58:23 +0700

    add signin and register

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,32 @@ async function saveDatabaseToJSONFile(n) {
   )
 }
 
+function searchUserbyEmail(email, password) {
+    const user = database.users.find(user => {
+        if (user.email === email && user.password === password) {
+            return user;
+            }
+        }
+    );
+    if (user === undefined) {
+        return "password or email is incorrect";
+    } 
+    return user;
+}
+
+function isUserNotExist(email) {
+    const user = database.users.find(user => {
+        if (user.email === email) {
+            return user;
+            }
+        }
+    );
+    if (user === undefined) {
+        return true;
+    } 
+    return false;
+}
+
 
 // body parser middleware very important to parse the body of the request
 // you can't use req.body without this middleware because it will be undefined
@@ -49,7 +75,6 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     console.log('Middleware 1');
-    console.log(req.body);
     next();
 })
 
@@ -62,23 +87,36 @@ app.get('/', (req, res) => {
 
 
 app.post('/signin' , (req,res) => {
-    res.json('signin')
-}) 
+    const {email, password} = req.body;
+    try {
+        res.json(searchUserbyEmail(email, password));
+    } catch (err) {
+        res.status(400).json('error logging in');
+    }
+  }
+)
 
 app.post('/register', (req,res) =>{
-    console.log(req.body);
     const {email, name, password} = req.body;
-    database.users.push({
-        id: database.users.length + 1,
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
-    });
-    res.json(database.users[database.users.length - 1]);
-    saveDatabaseToJSONFile(0);
-})
+    console.log("isUser Exist:" + isUserNotExist(email));
+    if (isUserNotExist(email)) {
+        database.users.push({
+            id: database.users.length + 1,
+            name: name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()
+        });
+        res.json(database.users[database.users.length - 1]);
+        saveDatabaseToJSONFile(0);}  
+    else {
+        res.json('user already exist');
+    }
+
+  }
+    
+)
 
 
 app.listen(3000, () => {
